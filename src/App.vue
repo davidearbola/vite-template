dc7f3b6efe9da9a5457ea98c388cdcd095c4371e
davidearbola <davidearbola@gmail.com>
2024-06-16 13:28:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+	data() {
+		return {
+			message: "Hello, new Project?",
+			dependencies: "Installed: Vue, Bootstrap, FontAwesome, Axios",
+		};
+	},
+};
+</script>
+
+<template>
+	<div
+		class="d-flex flex-column align-items-center justify-content-center vh-100 text-bg-dark"
+	>
+		<h1>
+			{{ message }}
+			<i
+				class="fa-regular fa-face-smile fa-beat"
+				style="color: #63e6be"
+			></i>
+		</h1>
+		<p>{{ dependencies }}</p>
+	</div>
+</template>
+
+<style scoped></style>
